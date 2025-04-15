ab24e8448c3a6f9eff76619af0fd830aa0fef810
c0c24056cb <c0c24056cb@edu.teu.ac.jp>
2025-04-15 17:08:46 +0900

    課題1 : 風に流れる

--- pygame_image.py
+++ pygame_image.py
@@ -19,7 +19,10 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        dx , dy = -1, 0
         key_lst = pg.key.get_pressed()#練習10-3
+        tori_rct.move_ip((dx, dy))
         if key_lst[pg.K_UP]:
             tori_rct.move_ip((0, -1))
         
@@ -30,7 +33,7 @@ def main():
             tori_rct.move_ip((-1, 0))
 
         if key_lst[pg.K_RIGHT]:
-            tori_rct.move_ip((+1, 0))
+            tori_rct.move_ip((+2, 0))
         x = tmr%3200#練習6/9
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2, [-x + 1600, 0])#練習7
